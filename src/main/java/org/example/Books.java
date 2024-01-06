f31453b2e8ed46a5a5a5947b9e484bcb5a4d12ed
pc <poyatajhiz59@gmail.com>
2024-01-06 07:42:36 -0800

    add getter and setter methods for books class

--- src/main/java/org/example/Books.java
+++ src/main/java/org/example/Books.java
@@ -14,4 +14,36 @@ public class Books {
         this.yearOfPublish = yearOfPublish;
         this.writer = writer;
     }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public int getYearOfPublish() {
+        return yearOfPublish;
+    }
+
+    public void setYearOfPublish(int yearOfPublish) {
+        this.yearOfPublish = yearOfPublish;
+    }
+
+    public int getWriter() {
+        return writer;
+    }
+
+    public void setWriter(int writer) {
+        this.writer = writer;
+    }
 }
