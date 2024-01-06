82a09785cb346b35d74dd00df26f72fefdca3ec0
pc <poyatajhiz59@gmail.com>
2024-01-06 07:46:17 -0800

    add constructor for writer class

--- src/main/java/org/example/Writer.java
+++ src/main/java/org/example/Writer.java
@@ -5,5 +5,15 @@ public class Writer {
     private String firstname;
     private String lastname;
     private int age;
-    books [] books;
+    Books[] books;
+
+    public Writer() {
+    }
+
+    public Writer(String firstname, String lastname, int age, Books[] books) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.books = books;
+    }
 }
